3f6ca56d446ea7caa2349cba6c82b7aedad425b0
Rahul Kumar <76434100+iamrahul25@users.noreply.github.com>
2026-02-14 11:16:56 +0530

    Add root route and modify health route response

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/health', (req, res) => {
+app.get('/', (req, res) => {
   res.send(`Server is Running on port ${port}`);
+});
+
+app.get('/health', (req, res) => {
+  res.send(`/health Route (Server is Running): ${port}`);
 });
 
 app.listen(port, () => {
